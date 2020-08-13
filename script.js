3999831176a8658d3ec1c98d54957165f5fa37f1
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-08-13 10:50:45 -0400

    Set up HTML sections and JS with parallax; added some initial styling

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // There are multiple - so this would return an array? console.log it
+    const parallaxEls = $('div.parallax');
+    console.log(parallaxEls);
+
+    parallaxEls.parallax();
+
+
+
+
+
+});
