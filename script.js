2776efdb5694f14a95f35e0382f11f24515f3012
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-08-16 02:15:30 -0400

    Fixed about me and project sections; added icons with links to contact section

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
 $(document).ready(function() {
-    // There are multiple - so this would return an array? console.log it
+    // There are multiple - so this would return an array
     const parallaxEls = $('div.parallax');
-    console.log(parallaxEls);
-
     parallaxEls.parallax();
 
+    // Variables for DOM elements
+    const portfolioContainerEl = $('section.portfolio-container');
+    const portfolioCardEl = $('div.portfolio-card');
+
+const populatePortfolio = (portfolioObj) => {
+    // INside the template to be cloned
+    const $portfolioTemplate = $(document.getElementById('portfolio-template').innerHTML);
+    // Create a temporary holder
+    const $fragContainer = $(document.createDocumentFragment());
+
+    for (key in portfolioObj) {
+        const $clone = $portfolioTemplate.clone();
+        const objKey =portfolioObj[key];
+        console.log(objKey);
+
+        // Insert the correct image, the project title, the links and the description
+        $clone.find('img.card-img').attr('src', objKey.image).attr('alt', `${objKey.name} gif`);
+        $clone.find('span.title').html(`${objKey.name}<i class="material-icons right">more_vert</i>`);
+        $clone.find('a.deployed').attr('href', objKey.deployed);
+        $clone.find('a.repo').attr('href', objKey.repo);
+        $clone.find('p.description').text(objKey.description);
+        $clone.find('p.tech-used').text(`Tech Used: ${objKey.techUsed}`);
+
+        // Append the clone to the frag container and then append that to the portfolio container
+        $fragContainer.append($clone);
+        portfolioContainerEl.append($fragContainer);
+    }
+};
+
+
+populatePortfolio(portfolioMaterials);
 
 
 
