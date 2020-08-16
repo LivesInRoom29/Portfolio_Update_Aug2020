9ae704d2d4a4404bbdbad92b19cc8198e77acc50
LIR29 <61219066+LivesInRoom29@users.noreply.github.com>
2020-08-16 02:12:51 -0400

    Added object for portfolio materials

---
+++ Assets/gitHubLinks.js
@@ -0,0 +1,34 @@
+const portfolioMaterials = {
+    project1: {
+        name: "High Card Picks Dinner",
+        repo: "https://github.com/LivesInRoom29/high-card-picks-dinner",
+        deployed: "https://tiffany-brand.github.io/high-card-picks-dinner/",
+        image: "./Assets/Img/highCard.gif",
+        description: "A relationship saver! This app helps couples, friends, and roommates fairly choose who picks dinner, and find quality dinner choices.",
+        techUsed: "HTML, CSS, Materialize, JavaScript, jQuery, Moment.js, Animate.CSS, Chart.js, and multiple API calls",
+    },
+    weatherDashboard: {
+        name: "Weather Dashboard",
+        repo: "https://github.com/LivesInRoom29/Weather_Dashboard",
+        deployed: "https://livesinroom29.github.io/Weather_Dashboard/",
+        image: "./Assets/Img/weatherDash2.jpg",
+        description: "Allows the user obtain weather information for any city. Retrieves data from the OpenWeather API, and displays the current weather conditions and a five day forecast.",
+        techUsed: "HTML, CSS, Bulma, JavaScript, jQuery, jQueryUI, Moment.js, and multiple API calls",
+    },
+    dailyPlanner: {
+        name: "Daily Planner",
+        repo: "https://github.com/LivesInRoom29/Daily_Planner",
+        deployed: "https://github.com/LivesInRoom29/Daily_Planner",
+        image: "./Assets/Img/plannerScreenShot.jpg",
+        description: "A daily calendar that provides a way for the user to plan their work day; saves calendar input in local storage.",
+        techUsed: "HTML, JavaScript, jQuery, Moment.js",
+    },
+    codeQuiz: {
+        name: "Code Quiz",
+        repo: "https://github.com/LivesInRoom29/Code_Quiz",
+        deployed: "https://livesinroom29.github.io/Code_Quiz",
+        image: "./Assets/Img/codeQuiz.gif",
+        description: "A timed, multiple choice coding quiz that stores and displays high scores.",
+        techUsed: "HTML, CSS, JavaScript - setInterval(), clearInterval(), saving to local storage and working with JSON objects to store and display saved data.",
+    },
+}
